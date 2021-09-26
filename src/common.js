833dca740789dc0b0fd04b5343f6ec58a9af5b47
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2021-09-26 13:52:52 +0200

    use stage frontend for local dev

--- src/common.js
+++ src/common.js
@@ -2,7 +2,7 @@ import {GraphQLClient} from 'graphql-request';
 
 export const baseUrl = new URL(document.currentScript.src).origin;
 
-export const apiUrl = window.location.hostname === 'localhost' ? 'https://keystone-next.docker/api/graphql' : `${baseUrl}/api/v1`;
+export const apiUrl = window.location.hostname === 'localhost' ? 'https://stage-breg.coverified.info/api/v1/' : `${baseUrl}/api/v1`;
 
 export const frontendUrl = window.location.hostname === 'localhost' ? '' : `${baseUrl}/frontend/v1`;
 
